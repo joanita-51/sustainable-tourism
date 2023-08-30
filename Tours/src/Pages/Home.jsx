d26be1b35f3625fb71c5e375c49fbb3d7830cf10
joanita-51 <nakityojoanita51@gmail.com>
2023-08-30 12:04:21 +0300

    Adding responsiveness and resizing images

--- Tours/src/Pages/Home.jsx
+++ Tours/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import backgroundImage from '../assets/images/giraffe.jpg'
 import camping from '../assets/images/banner2.jpg'
 import kit from '../assets/images/FirstAidKit.jpg'
@@ -10,28 +10,51 @@ import deers from  '../assets/images/deers.jpg'
 import {FaQuoteLeft} from 'react-icons/fa'
 import { TourismData, ToursimColumns } from '../data'
 import DataTable from 'react-data-table-component'
+import {FiMenu} from 'react-icons/fi'
 function Home() {
     const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    const toggleNav = ()=>{
+        setIsNavOpen(!isNavOpen)
+    }
   return (
     <>
         {/* Hero section */}
         <div id="home" className="bg-cover bg-center h-screen" style={{backgroundImage: `url(${backgroundImage})`}}>
             <div className=" bg-[#040D12] bg-opacity-60 absolute inset-0">
-                <div className='flex justify-between text-white px-10 pt-5'>
-                    {/* Left Side (Navbar) */}
-                    <div className="">
-                        <h1 className="text-2xl font-bold uppercase tracking-tighter">Uganda Tourism</h1>
-                        <p className='text-lg uppercase tracking-widest'>Adventures</p>
+                <div className='container mx-auto flex justify-between items-center px-4 text-white md:px-10 py-5'>
+                    {/* Hamburger Menu (for small screens) */}
+                    <div className="md:hidden">
+                        <button className="text-white" onClick={toggleNav}>
+                            <FiMenu size={30}/>
+
+                        </button>
+                        <ul className={`${isNavOpen?'flex space-x-6 items-center':'hidden'}`}>
+                            <li className='hover:text-[#EB1D36]'><a href="#home">Home</a></li>
+                            <li className='hover:text-[#EB1D36]'><a href="#about">About</a></li>
+                            <li className='hover:text-[#EB1D36]'><a href="#services">Services</a></li>
+                            <li className='hover:text-[#EB1D36]'><a href="#upcoming">Upcoming</a></li>
+                            <button className="bg-[#EB1D36] hover:bg-red-600 text-white font-bold py-2 px-4 uppercase rounded-full">
+                                <a href="#contact">Contact Us</a>
+                            </button>
+                        </ul>
+                        
                     </div>
-                    
+                    {/* Left Side (Navbar) */}
+                    <div className="hidden md:block">
+                        <h1 className="text-2xl font-bold uppercase tracking-tighter">E - Toursim</h1>
+                        <p className='text-lg uppercase tracking-widest'>UGANDA</p>
+                    </div>
+
+                   
                     {/* Right Side (Navigation Links) */}
-                    <div className="">
-                        <nav className="flex items-center space-x-8 text-lg">
+                    <div className={`hidden md:block`}>
+                        <nav className="md:flex items-center space-x-8 text-lg">
                             <ul className="flex space-x-8">
-                                <li><a href="#home">Home</a></li>
-                                <li><a href="#about">About</a></li>
-                                <li><a href="#services">Services</a></li>
-                                <li><a href="#upcoming">Upcoming</a></li>
+                                <li className='hover:text-[#EB1D36]'><a href="#home">Home</a></li>
+                                <li className='hover:text-[#EB1D36]'><a href="#about">About</a></li>
+                                <li className='hover:text-[#EB1D36]'><a href="#services">Services</a></li>
+                                <li className='hover:text-[#EB1D36]'><a href="#upcoming">Upcoming</a></li>
                             </ul>
                             <button className="bg-[#EB1D36] hover:bg-red-600 text-white font-bold py-2 px-4 uppercase rounded-full">
                                 <a href="#contact">Contact Us</a>
@@ -46,7 +69,9 @@ function Home() {
                         </p>
                         <p className="bg-[#EB1D36] h-[2px] w-20 mx-auto mt-2"></p>
                         <p className="uppercase font-bold text-5xl tracking-widest my-10">The Pearl of Africa</p>
-                        <button className="bg-[#EB1D36] rounded-full px-5 py-2 mt-5 text-white text-lg">Learn more</button>
+                        <button className="bg-[#EB1D36] hover:bg-[#C31228] transition duration-300 ease-in-out rounded-full px-5 py-2 mt-5 text-white text-lg">
+                           <a href="#services">Learn more</a> 
+                        </button>
                     </div>
                 </div>
 
@@ -80,7 +105,7 @@ function Home() {
                             <p className="text-gray-700 text-sm">Enhance your outdoor adventures with high-quality camping bags for essential item storage.</p>
                         </div>
                         <div className="px-6 py-4">
-                            <a href="#" className="bg-[#EB1D36] text-white font-bold py-2 px-4 rounded-full">
+                            <a href="#" className="bg-[#EB1D36] hover:bg-[#C31228] transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-full">
                                 Order Now
                             </a>
                         </div>
@@ -94,7 +119,7 @@ function Home() {
                         <p className="text-gray-700 text-sm">Get one of our essential first aid kits for safety during your trips.</p>
                         </div>
                         <div className="px-6 py-4">
-                        <a href="#" className="bg-[#EB1D36] text-white font-bold py-2 px-4 rounded-full">
+                        <a href="#" className="bg-[#EB1D36] hover:bg-[#C31228] transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-full">
                             Order Now
                         </a>
                         </div>
@@ -108,7 +133,7 @@ function Home() {
                             <p className="text-gray-700 text-sm">Rent our comfortable and reliable traveling cars for your journeys.</p>
                             </div>
                             <div className="px-6 py-4">
-                            <a href="#" className="bg-[#EB1D36] text-white font-bold py-2 px-4 rounded-full">
+                            <a href="#" className="bg-[#EB1D36] hover:bg-[#C31228] transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-full">
                                 Order Now
                             </a>
                             </div>
@@ -119,41 +144,51 @@ function Home() {
 
         {/* The about us section  */}
         <section class="bg-white py-24" id='about'>
-            <div class=" mx-16 grid grid-cols-3 space-x-20">
+            <div class=" mx-16 grid md:grid-cols-3 space-x-20">
                 <div className=''>
-                    <h2 class="text-3xl font-bold mb-6 text-left">ABOUT US</h2>
-                    <p className="bg-[#EB1D36] h-[2px] w-20 mt-2 mb-12"></p>
-                    <p class="text-gray-700 mb-4 text-justify">
-                        Welcome to <span class="text-[#EB1D36] font-semibold">UGANDA TOURS</span>, where we make your Uganda adventure amazing!
+                    <h2 class="text-3xl font-bold mb-6 text-center md:text-left">ABOUT US</h2>
+                    <p className="bg-[#EB1D36] h-[2px] m-auto md:m-0 w-20 mt-2 mb-12 md:mb-6"></p>
+                    <p class="text-gray-700 mb-4 text-justify text-sm lg:text-base">
+                        Welcome to <span class="text-[#EB1D36] font-semibold">E-Tourism Uganda</span>, where we make your Uganda adventure amazing!
                     </p>
-                    <p class=" text-gray-700 mb-4 text-justify">
+                    <p class=" text-gray-700 mb-4 text-justify text-sm lg:text-base">
                         We love Uganda's beauty and culture. Our goal is to help you explore it sustainably.
                     </p>
-                    <p class=" text-gray-700 mb-4 text-justify">
+                    <p class=" text-gray-700 mb-4 text-justify text-sm lg:text-base">
                         Our team is full of Uganda enthusiasts. We're here to assist you whether you want a wildlife safari, camping gear, or a tourist van for comfort.
                     </p>
-                    <p class=" text-gray-700 mb-8 text-justify">
+                    <p class=" text-gray-700 mb-8 text-justify text-sm lg:text-base">
                         We're all about responsible travel, so your adventure is memorable and eco-friendly. Join us to uncover Uganda's heart and create your unique story.
                     </p>
-                    <a href="#services" class="bg-[#EB1D36] text-white font-bold py-3 px-8 rounded-full hover:bg-[#C31228] transition duration-300 ease-in-out block text-center md:inline-block">
-                        Check out our services
-                    </a>
-                </div>
-                <div class="col-span-2">
-                    <div className='flex space-x-5'>
-                        <div className='flex flex-col space-y-5'>
-                            <img src={cows} alt='Cows' className=' rounded-xl w-full'/>
-                            <img src={monkeys} alt="Uganda Tours" className='rounded-xl h-[400px] w-full object-cover object-bottom'/>     
-
-                        </div>
-                        <div className='flex flex-col space-y-5'>
-                            <img src={deers} alt="Deers" className='rounded-xl h-[400px] w-full object-cover object-bottom'/>
-                            <img src={rhinos} alt="Rhinos" className='rounded-xl' />
-                        </div>
-
+                    <div className='flex justify-center md:justify-start text-sm lg:text-base'>
+                        <a href="#services" class="bg-[#EB1D36] text-white font-bold py-3 px-8 rounded-full hover:bg-[#C31228] transition duration-300 ease-in-out block text-center md:inline-block ">
+                            Check out our services
+                        </a>
                     </div>
 
                 </div>
+                <div class="md:col-span-2 pt-12 md:pt-0">
+                    <div className='md:flex md:space-x-5 space-y-5'>
+                        <div className='flex flex-col space-y-5'>
+                            <img src={cows} alt='Cows' className='rounded-xl w-full' />
+
+                            {/* Use aspect-ratio to maintain aspect ratio */}
+                            <div className="aspect-w-16 aspect-h-2 hidden md:block">
+                                <img src={monkeys} alt="Uganda Tours" className='rounded-xl object-cover object-bottom' />
+                            </div>
+                        </div>
+                        
+                        <div className='flex flex-col space-y-5'>
+                            {/* Use aspect-ratio to maintain aspect ratio */}
+                            <div className="aspect-w-16 aspect-h-9">
+                                <img src={deers} alt="Deers" className='rounded-xl object-cover object-bottom' />
+                            </div>
+
+                            <img src={rhinos} alt="Rhinos" className='rounded-xl' />
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </section>
@@ -175,8 +210,8 @@ function Home() {
         {/* Footer Section */}
         <div className='bg-[#191919] py-24' id='contact'>
             <div className='container mx-auto text-center text-white'>
-                <p>123 Fifth Avenue, Kampala, Uganda | Phone: 256-123-456-3013 | Email: Uganda@Tours.com</p>
-                <p className='mt-5'>Copyright © 2023 Uganda Tours</p>
+                <p>123 Fifth Avenue, Kampala, Uganda | Phone: 256-123-456-3013 | Email: E-Toursim@Uganda.com</p>
+                <p className='mt-5'>Copyright © 2023 E-Tourism Uganda</p>
             </div>
 
         </div>
